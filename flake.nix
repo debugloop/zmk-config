ea6058cd8b48206d015965153de009f64423af6e
Woodpecker <git@danieln.de>
2026-01-26 01:01:11 +0100

    update zmk dependencies

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
         # parts = ["left" "right"];
         # parts = ["dongle"];
 
-        zephyrDepsHash = "sha256-0ni/3FJJizCqvp0X2tmwu29eWVqgd3hRXGOjtW6OiUE=";
+        zephyrDepsHash = "";
 
         meta = {
           description = "ZMK firmware";
